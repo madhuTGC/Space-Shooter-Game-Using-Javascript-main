de472f4627e33e9354e014325717ca59ce876c5f
madhuTGC <madhu@thegamificationcompany.com>
2024-01-05 15:55:18 +0530

    +1

--- app.js
+++ app.js
@@ -1,3 +1,52 @@
+const QuestionList = [
+  { 'Q1': "What is Your Name" },
+  { 'Q2': "What is Your School Name" },
+  { 'Q3': "What is Your father Name" },
+  { 'Q4': "What is Your Mother Name" },
+  { 'Q5': "What is Your Brother Name" },
+  { 'Q6': "Where are you From" },
+  { 'Q7': "What is going on?" },
+  { 'Q8': "Had a Dinner?" },
+  { 'Q9': "why 1+1 is equal to 2?" },
+  {'Q10': "Explain Newtons law of motion?"}
+
+
+  // Add more questions as needed
+];
+
+let currentQuestionIndex = 0;
+
+
+function displayQuestion() {
+  if (currentQuestionIndex < QuestionList.length) {
+      const currentQuestion = QuestionList[currentQuestionIndex];
+      displayQuestionInModal(Object.values(currentQuestion)[0]);
+      
+      currentQuestionIndex++;
+  } else {
+      currentQuestionIndex = 0;
+      alert("madhu")
+      // onGameOver();
+  }
+}
+
+function displayQuestionInModal(question) {
+  $('#questionText').text(question);
+  $('#continueButton').on('click', function () {
+      // Close the modal
+      $('#questionModal').modal('hide');
+      // Resume the game logic here
+      // resumeGame();
+  });
+
+  $('#questionModal').modal('show');
+}
+
+
+
+
+
+
 var jet = document.getElementById("jet");
 var board = document.getElementById("board");
 
@@ -7,7 +56,7 @@ window.addEventListener("keydown", (e) => {
     jet.style.left = left - 10 + "px";
   }
   //460  =>  board width - jet width
-  else if (e.key == "ArrowRight" && left <= 460) {
+  else if (e.key == "ArrowRight" && left <= 1500) {
     jet.style.left = left + 10 + "px";
   }
 
@@ -36,9 +85,13 @@ window.addEventListener("keydown", (e) => {
             bulletbound.bottom <= rockbound.bottom
           ) {
             rock.parentElement.removeChild(rock); //Just removing that particular rock;
+            // alert("madhu")
+            displayQuestion();
+            console.log("1",1) 
+
             //Scoreboard
-            document.getElementById("points").innerHTML =
-              parseInt(document.getElementById("points").innerHTML) + 1;
+            // document.getElementById("points").innerHTML =
+              // parseInt(document.getElementById("points").innerHTML) + 1;
           }
         }
       }
@@ -47,7 +100,7 @@ window.addEventListener("keydown", (e) => {
       );
 
       //Stops the bullet from moving outside the gamebox
-      if (bulletbottom >= 500) {
+      if (bulletbottom >= 1000) {
         clearInterval(movebullet);
       }
 
@@ -65,13 +118,31 @@ var generaterocks = setInterval(() => {
     window.getComputedStyle(rock).getPropertyValue("left")
   );
   //generate value between 0 to 450 where 450 => board width - rock width
-  rock.style.left = Math.floor(Math.random() * 450) + "px";
+  rock.style.left = Math.floor(Math.random() * 2000) + "px";
+
 
   board.appendChild(rock);
 }, 1000);
 
+
+// var generaterocksright = setInterval(() => {
+//   var rock = document.createElement("div");
+//   rock.classList.add("rocks");
+//   //Just getting the left of the rock to place it in random position...
+//   var rockright = parseInt(
+//     window.getComputedStyle(rock).getPropertyValue("right")
+//   );
+//   //generate value between 0 to 450 where 450 => board width - rock width
+//   rock.style.right = Math.floor(Math.random() * 1000) + "px";
+
+
+//   board.appendChild(rock);
+// }, 1000);
+
 var moverocks = setInterval(() => {
   var rocks = document.getElementsByClassName("rocks");
+  console.log("rocks",rocks.length)
+
 
   if (rocks != undefined) {
     for (var i = 0; i < rocks.length; i++) {
@@ -82,12 +153,20 @@ var moverocks = setInterval(() => {
       );
       //475 => boardheight - rockheight + 25
       if (rocktop >= 475) {
-        alert("Game Over");
-        clearInterval(moverocks);
-        window.location.reload();
+        // alert("Game Over");
+        // clearInterval(moverocks);
+        // window.location.reload();
       }
 
-      rock.style.top = rocktop + 25 + "px";
+      rock.style.top = rocktop + 40 + "px";
     }
   }
-}, 450);
+}, 1000);
+
+
+
+
+
+
+
+
